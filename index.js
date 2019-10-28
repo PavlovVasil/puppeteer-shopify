40831edb31d635ac4b312a48dca287bc6f55d0ca
PavlovVasil <vasil@vasilpavlov.com>
2019-10-28 21:16:09 +0200

    Get sections and divs with experts

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const puppeteer = require('puppeteer');
 
 (async () => {
     try {
-        const browser = await puppeteer.launch({ headless: true });
+        const browser = await puppeteer.launch({ headless: false });
         const page = await browser.newPage();
         page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36');
         await page.goto('https://experts.shopify.com/');
         //this gets the four sections with experts
-        await page.waitForSelector('._1_tev:nth-child(3) ._2fA-Y .XT3B4 ')
+        await page.waitForSelector('._17BF0');
+        const sections = await page.$$('._17BF0');
+        console.log(sections.length);
+        for (const section of sections) {
+            const divs = await section.$$('._3OHLp > div');
+            console.log(divs.length)
+        }
     
     } catch (e) {
         console.log(`our error: ${e}`)
