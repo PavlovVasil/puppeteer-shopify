49194da980c3cd3db9091a27dfcdf7a9602fb383
PavlovVasil <vasil@vasilpavlov.com>
2019-10-29 23:23:25 +0200

    Getting hrefs from the divs

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
 const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
 
 (async () => {
     try {
+        const baseUrl = 'https://experts.shopify.com/'
         const browser = await puppeteer.launch({ headless: false });
         const page = await browser.newPage();
         
         page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36');
         
-        await page.goto('https://experts.shopify.com/');
+        await page.goto(baseUrl);
         await page.waitForSelector('._17BF0');
-        const sections = await page.$$('._17BF0');
-        
-        console.log(sections.length);
-        
-        for (const section of sections) {
-            await page.goto('https://experts.shopify.com/');
-            await page.waitForSelector('._17BF0');
-            
-            const divs = await section.$$('._3OHLp > div');
-            console.log(divs.length)
-            
-            for (const div of divs) {
-                div.click()
-                await page.waitForSelector('._2fA-Y')
-            }
+        let content = await page.content();
+        const $ = cheerio.load(content)
+        const hrefs = []
+        const list = ($('._17BF0 ._3OHLp > div a'))
+        for (let i = 0; i < list.length; i++) {
+            hrefs.push($(list[i]).attr('href'))
         }
-    
+        
     } catch (e) {
         console.log(`our error: ${e}`)
     }
