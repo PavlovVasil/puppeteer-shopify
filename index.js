f7304758205a03a4277fcb9731fe909c697655e2
PavlovVasil <vasil@vasilpavlov.com>
2019-10-30 21:37:07 +0200

    Cast HTMLCollection to array before calling map

--- index.js
+++ index.js
@@ -8,16 +8,22 @@ const cheerio = require('cheerio');
         const page = await browser.newPage();
         
         page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36');
-        
+        const result = [];
         await page.goto(baseUrl);
         await page.waitForSelector('._17BF0');
         let content = await page.content();
         const $ = cheerio.load(content)
-        const hrefs = []
-        const list = ($('._17BF0 ._3OHLp > div a'))
-        for (let i = 0; i < list.length; i++) {
-            hrefs.push($(list[i]).attr('href'))
-        }
+        
+        //constructing all urls to be scraped
+        const linkUrls = Array.from($('._17BF0 ._3OHLp > div a')).map(elem => `${baseUrl}${$(elem).attr('href')}`)
+        
+        linkUrls.forEach(async link => {
+            const detailsPage = await browser.newPage();
+            await browser.goto(link);
+            await detailsPage.waitForSelector('_12oDh');
+            result.concat()
+            debugger
+        })
         
     } catch (e) {
         console.log(`our error: ${e}`)
