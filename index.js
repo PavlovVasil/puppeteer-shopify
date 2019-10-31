f49ef8cbcf656a500861080edc336e6e729d29cf
PavlovVasil <vasil@vasilpavlov.com>
2019-10-31 21:33:00 +0200

    add check for last page when scraping

--- index.js
+++ index.js
@@ -3,32 +3,36 @@ const cheerio = require('cheerio');
 
 (async () => {
     try {
-        const baseUrl = 'https://experts.shopify.com/'
+        //const baseUrl = 'https://experts.shopify.com'
+        const baseUrl = 'https://experts.shopify.com/services/visual-content-and-branding/develop-brand-look-and-feel?page=18'
         const browser = await puppeteer.launch({ headless: false });
         const page = await browser.newPage();
         
         page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36');
-        const result = [];
+         //constructing all urls to be scraped
+        const expertsUrls = [];
         await page.goto(baseUrl);
-        await page.waitForSelector('._17BF0');
+        //await page.waitForSelector('._17BF0');
         let content = await page.content();
         const $ = cheerio.load(content)
         
-        //constructing all urls to be scraped
-        const linkUrls = Array
-            .from($('._17BF0 ._3OHLp > div a'))
-            .map(elem => ({
-                url: `${baseUrl}${$(elem).attr('href')}`,
-                category: $(elem).find('.Polaris-Caption').text() 
-    }))
-        console.log(linkUrls)
-        linkUrls.forEach(async link => {
-            const detailsPage = await browser.newPage();
-            await browser.goto(link);
-            await detailsPage.waitForSelector('_12oDh');
-            result.concat()
-            debugger
-        })
+        // const sectionUrls = Array
+        //                     .from($('._17BF0 ._3OHLp > div a'))
+        //                     .map(elem => `${baseUrl}${$(elem).attr('href')}`)
+        // for (url of sectionUrls) {
+        //     const expertsListPage = await browser.newPage();
+        //     await browser.goto(link);
+        //     await expertsList.waitForSelector('_12oDh');
+        //     let content = await page.content();
+        //     const $ = cheerio.load(content)
+        //     const nextButton = $('.Polaris-Pagination__Button.Polaris-Pagination__NextButton').disabled
+        //     expertsUrls.concat()
+        // }
+
+        //this checks is the "next page" button is disabled - if it is, we are on the last page
+        const nextButton = $('button.Polaris-Pagination__Button.Polaris-Pagination__NextButton').prop("disabled")
+        console.log(nextButton)
+       //expertsUrls.concat()
         
     } catch (e) {
         console.log(`our error: ${e}`)
