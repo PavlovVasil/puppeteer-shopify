64d0a8fa6998423ec27dcd4522a73fd929403058
PavlovVasil <vasil@vasilpavlov.com>
2019-11-02 12:47:36 +0200

    Refactor the scraper

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
-const fs = require('fs');
+const fs = require('fs').promises;
+
+const saveUrlsToFile = async (urls = [], fName = "default.json") => {
+    await fs.writeFile(fName, JSON.stringify({urls: urls}));
+}
+
+const getUrlsForSection = async (sectionUrl) => {
+    const expertsListPage = await browser.newPage();
+    await browser.goto(sectionUrl);
+    await expertsList.waitForSelector('_12oDh');
+    const content = await expertsListPage.content();
+    const $ = cheerio.load(content)
+
+    //if the nextButton is disabled, we are on the last page of this section
+    const nextButton = $('.Polaris-Pagination__Button.Polaris-Pagination__NextButton').disabled
+    
+}
 
 (async () => {
     try {
         const baseUrl = 'https://experts.shopify.com';
-        //const baseUrl = 'https://experts.shopify.com/services/visual-content-and-branding/develop-brand-look-and-feel?page=18'
+        const baseSectionUrl = 'https://experts.shopify.com/services/visual-content-and-branding/develop-brand-look-and-feel?page=18';
+        
         const browser = await puppeteer.launch({ headless: false });
         const page = await browser.newPage();
-        
         page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36');
-         //constructing all urls to be scraped
-        //const expertsUrls = [];
         await page.goto(baseUrl);
-        //await page.waitForSelector('._17BF0');
         let content = await page.content();
         const $ = cheerio.load(content);
+        const sectionUrls = Array.from($('._17BF0 ._3OHLp > div a')).map(elem => `${baseUrl}${$(elem).attr('href')}`);
+        await saveUrlsToFile(sectionUrls, 'sectionUrls.json');
         
-        const sectionUrls = Array
-                            .from($('._17BF0 ._3OHLp > div a'))
-                            .map(elem => `${baseUrl}${$(elem).attr('href')}`);
-        console.log(sectionUrls);
-        const data = JSON.stringify(sectionUrls);
-        fs.writeFileSync('sectionUrls.json', data);
+
         // for (url of sectionUrls) {
         //     const expertsListPage = await browser.newPage();
         //     await browser.goto(link);
