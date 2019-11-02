4aa06499a5150831d9bb2f702910cee92ecb8763
PavlovVasil <vasil@vasilpavlov.com>
2019-11-02 14:51:42 +0200

    Getting all expert urls for a section

--- index.js
+++ index.js
@@ -6,22 +6,38 @@ const saveUrlsToFile = async (urls = [], fName = "default.json") => {
     await fs.writeFile(fName, JSON.stringify({urls: urls}));
 }
 
-const getUrlsForSection = async (sectionUrl) => {
-    const expertsListPage = await browser.newPage();
-    await browser.goto(sectionUrl);
-    await expertsList.waitForSelector('_12oDh');
-    const content = await expertsListPage.content();
-    const $ = cheerio.load(content)
-
-    //if the nextButton is disabled, we are on the last page of this section
-    const nextButton = $('.Polaris-Pagination__Button.Polaris-Pagination__NextButton').disabled
+const getExpertUrlsForSection = async (baseUrl, sectionUrl, browser) => {
+    let expertUrls = [];
+    const browserTab = await browser.newPage();
+    await browserTab.goto(sectionUrl);
+    await browserTab.waitForSelector('.kVWrj');
+    const content = await browserTab.content();
+    const $ = cheerio.load(content);
+    expertUrls = expertUrls.concat(Array.from($('.kVWrj a')).map(a => `${baseUrl}${$(a).prop('href')}`));
     
+    //if the nextButton is disabled, we are on the last page of this section
+    let nextButton = !$('.Polaris-Pagination__Button.Polaris-Pagination__NextButton').disabled
+    let nextPageQuery = "?page=";
+    let i = 2;
+    //Go to the next page and get the experts urls, until we don't have a "next" button 
+    while(nextButton){
+        const nextPageUrl = `${sectionUrl}${nextPageQuery}${i}`
+        await browserTab.goto(nextPageUrl);
+        await browserTab.waitForSelector('.kVWrj');
+        const content = await browserTab.content();
+        const $ = cheerio.load(content);
+        expertUrls = expertUrls.concat(Array.from($('.kVWrj a')).map(a => `${baseUrl}${$(a).prop('href')}`));
+        nextButton = !$('.Polaris-Pagination__Button.Polaris-Pagination__NextButton').disabled
+        console.log(expertUrls)
+        i++
+    }
+    return expertUrls
 }
 
 (async () => {
     try {
         const baseUrl = 'https://experts.shopify.com';
-        const baseSectionUrl = 'https://experts.shopify.com/services/visual-content-and-branding/develop-brand-look-and-feel?page=18';
+        const baseSectionUrl = 'https://experts.shopify.com/services/visual-content-and-branding/develop-brand-look-and-feel';
         
         const browser = await puppeteer.launch({ headless: false });
         const page = await browser.newPage();
@@ -29,24 +45,12 @@ const getUrlsForSection = async (sectionUrl) => {
         await page.goto(baseUrl);
         let content = await page.content();
         const $ = cheerio.load(content);
-        const sectionUrls = Array.from($('._17BF0 ._3OHLp > div a')).map(elem => `${baseUrl}${$(elem).attr('href')}`);
-        await saveUrlsToFile(sectionUrls, 'sectionUrls.json');
-        
 
-        // for (url of sectionUrls) {
-        //     const expertsListPage = await browser.newPage();
-        //     await browser.goto(link);
-        //     await expertsList.waitForSelector('_12oDh');
-        //     let content = await page.content();
-        //     const $ = cheerio.load(content)
-        //     const nextButton = $('.Polaris-Pagination__Button.Polaris-Pagination__NextButton').disabled
-        //     expertsUrls.concat()
-        // }
+        //this saves the section urls to a json file
+        // const sectionUrls = Array.from($('._17BF0 ._3OHLp > div a')).map(elem => `${baseUrl}${$(elem).attr('href')}`);
+        // await saveUrlsToFile(sectionUrls, 'sectionUrls.json');
 
-        //this checks is the "next page" button is disabled - if it is, we are on the last page
-        //const nextButton = $('button.Polaris-Pagination__Button.Polaris-Pagination__NextButton').prop("disabled")
-        
-        //console.log(nextButton)
+        await getExpertUrlsForSection(baseUrl, baseSectionUrl, browser);
         await browser.close()
     } catch (e) {
         console.log(`our error: ${e}`)
